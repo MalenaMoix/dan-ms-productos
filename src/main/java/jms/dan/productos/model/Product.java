27f6615742a50761e1d712fa364b7d285d8b8e19
MalenaMoix <moixmalena@gmail.com>
2021-06-17 19:35:32 -0300

    Fix Cola de pedidos

--- src/main/java/jms/dan/productos/model/Product.java
+++ src/main/java/jms/dan/productos/model/Product.java
@@ -1,9 +1,6 @@
 package jms.dan.productos.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Product {
